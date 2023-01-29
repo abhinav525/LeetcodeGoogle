46b7fcec0d66b7c457f5d63093b161885bd8ba91
ABHINAV <78312043+abhinav525@users.noreply.github.com>
2023-01-29 06:15:17 +0530

    Create 904. Fruit Into Baskets.cpp

---
+++ 904. Fruit Into Baskets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        set<int> s;
+        for(int i: fruits) s.insert(i);
+        if(s.size()<=2) return fruits.size();
+        fruits.push_back(100001);
+        int n=fruits.size();
+        int j=0;
+        int total=0;
+        vector<int>f(100005);//freq vector
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(f[fruits[i]]==0 && total==2){
+                maxi=max(maxi,i-j);///subarray to maximize the answer
+
+                while(j<i){
+                    if(f[fruits[j]]>1) f[fruits[j]]--,j++;
+                    else if(f[fruits[j]]==1){
+                        f[fruits[j]]--;
+                        j++;
+                        break;
+                    }
+                }
+                f[fruits[i]]++;
+            }
+            else if(f[fruits[i]]==0){
+                total++;
+                f[fruits[i]]++;
+
+            }
+            else f[fruits[i]]++;
+        }
+        return maxi;
+        
+    }
+};
